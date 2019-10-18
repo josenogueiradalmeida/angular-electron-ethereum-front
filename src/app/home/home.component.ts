9617d44c93731834d8f3d9fb0a68772321d19a06
José Nogueira <josej@bndes.gov.br>
2019-10-18 19:02:38 -0300

    Faz o web3 funcionar no Front

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,10 @@ import Web3 from 'web3';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+
+  resultado: any = "lele";
 
   constructor(private http: HttpClient) {
 
@@ -17,10 +20,19 @@ export class HomeComponent implements OnInit {
     
   }
 
-  lala() {
+  getBlockchainAccounts() {
     console.log('oi')
     const web3 = new Web3('ws://localhost:9545');
     console.log(web3)
+    web3.eth.getAccounts(function(error, accounts) {
+                if (error) 
+                  console.log(error)
+                else {
+                  console.log("deu certo")
+                }
+                  this.resultado = "lala"
+                  console.log(accounts)
+            })
     //let retorno = this.http.get<Object>("http://localhost:9090/api/read")    
     //console.log(retorno)
   }
